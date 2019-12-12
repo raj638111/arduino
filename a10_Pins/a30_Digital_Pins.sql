20edc01cc902e3e3a692880eac7fd0c3ef56c08c
Rajkumar Gunasekaran <rgunasekaran@homeaway.com>
2019-12-12 17:59:31 -0600

    -

--- a10_Pins/a30_Digital_Pins.sql
+++ a10_Pins/a30_Digital_Pins.sql
@@ -1,5 +1,11 @@
 
--- Header
+-- High (1) / Low (0)
+
+  # In Arduino,
+    + High = ~ 3.5V - 5V
+    + Low  = ~   0V - 3.0V
+
+-- Digital Pins
 
   # Large Header
 
@@ -12,10 +18,4 @@
         . Analog Input Pins or
         . Digital I/O pins
 
--- Other info
-
-  # Ground Pins
-    + 3 Pins
-  # Digital I/O Pins
-    + 20 Pins
 
---
+++ a10_Pins/z10_Other.sql
@@ -0,0 +1,8 @@
+
+-- Other info
+
+  # Ground Pins
+    + 3 Pins
+  # Digital I/O Pins
+    + 20 Pins
+
